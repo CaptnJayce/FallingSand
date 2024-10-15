2f0d6f3ae8d2f4f49b4b2dbc599901e62ef8a2a0
CaptnJayce <captn.dev@protonmail.com>
2024-10-15 20:57:12 +0100

    basic sand falling

--- main.cpp
+++ main.cpp
@@ -1,24 +1,60 @@
 #include "raylib.h"
 #include <iostream>
 
-int arr[5][5] {
-  {0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0}
-};
+enum TileType { EMPTY = 0, BLOCK = 1, SAND = 2 };
+
+bool IsBlockBelow(int tilemap[][5], int rows, int cols, int row, int col) {
+  return (row + 1 < rows) && (tilemap[row + 1][col] == BLOCK);
+}
 
 int main() {
-  const int screenWidth = 1200;
-  const int screenHeight = 800;
+  const int tileHeight = 32;
+  const int tileWidth = 32;
 
-  InitWindow(screenWidth, screenHeight, "Falling Sand");
+  const int rows = 5;
+  const int cols = 5;
 
-  while(WindowShouldClose() == false){
-    std::cout << "do some shit" << std::endl;
+  int tilemap[rows][cols]{{0, 2, 0, 2, 0},
+                          {0, 0, 0, 0, 0},
+                          {0, 0, 0, 0, 0},
+                          {0, 0, 1, 1, 0},
+                          {0, 0, 0, 0, 0}};
+
+  InitWindow(600, 600, "falling sand");
+  SetTargetFPS(60);
+
+  while (WindowShouldClose() == false) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    if (IsKeyDown(KEY_SPACE)) {
+      for (int row = rows - 1; row >= 0; row--) {
+        for (int col = 0; col < cols; col++) {
+          if (tilemap[row][col] == SAND) {
+            if (!IsBlockBelow(tilemap, rows, cols, row, col) &&
+                tilemap[row + 1][col] == EMPTY) {
+              tilemap[row + 1][col] = SAND;
+              tilemap[row][col] = EMPTY;
+            }
+          }
+        }
+      }
+    }
+
+    for (int row = 0; row < rows; row++) {
+      for (int col = 0; col < cols; col++) {
+        int x = col * tileWidth;
+        int y = row * tileHeight;
+
+        if (tilemap[row][col] == BLOCK) {
+          DrawRectangle(x, y, tileWidth, tileHeight, WHITE);
+        } else if (tilemap[row][col] == SAND) {
+          DrawRectangle(x, y, tileWidth, tileHeight, YELLOW);
+        }
+      }
+    }
+    EndDrawing();
   }
 
   CloseWindow();
 }
-
